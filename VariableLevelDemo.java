f9cda539c3454a95eb743e2591b88fe35c086565
kavya <30796742+kavyavmanu@users.noreply.github.com>
2021-04-29 21:01:22 +0530

    Create VariableLevelDemo.java

---
+++ VariableLevelDemo.java
@@ -0,0 +1,27 @@
+class VariableLevelDemo{
+	
+	int var;
+
+	boolean b;
+	VariableLevelDemo temp;
+
+	public static void main(String[] args) {
+
+	
+
+		for(int v = 3; v > 10; v++){
+			System.out.println(v);
+		}
+		System.out.println(v); 
+	}
+
+	void doSomething(){
+		
+		int value;
+	}
+
+	void met(){
+		char ch;
+	}
+
+}
