b71727e582636b6cf4b156f6afcdef3bfce697c2
kavya <30796742+kavyavmanu@users.noreply.github.com>
2021-04-29 20:56:24 +0530

    Create  MultiplyByUsingMethodsWithParametersAndRetrunType.java

---
+++ MultiplyByUsingMethodsWithParametersAndRetrunType.java
@@ -0,0 +1,18 @@
+class MultiplyByUsingMethodsWithParametersAndRetrunType{
+	public static void main(String[] args) {
+		java.util.Scanner theScan = new java.util.Scanner(System.in);
+
+		System.out.println("Enter two numbers to multiply:");
+
+		int firstValue = theScan.nextInt();	
+		int secondValue = theScan.nextInt();
+
+		new MultiplyByUsingMethodsWithParametersAndRetrunType().multiply(firstValue, secondValue);
+	}
+
+	void multiply(int firstValue, int secondValue){
+		int result = firstValue * secondValue;
+
+		System.out.println("Result is : " + result);
+	}
+}
